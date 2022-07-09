f37089edaaf71d1e9e66b15519069a8037ebc42b
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-09 12:50:45 +0500

    logger interface /driver

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ahmed-deftoner/MonkeDB
 
 go 1.17
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
--- main.go
+++ main.go
@@ -3,7 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 )
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, ...interface{})
+		Debug(string, ...interface{})
+		Trace(string, ...interface{})
+	}
+
+	Driver struct {
+		mutex   sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir     string
+		log     Logger
+	}
+)
+
+type Options struct {
+	Logger
+}
 
 type Address struct {
 	City    string
